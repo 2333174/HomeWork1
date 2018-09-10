700afbc8a4ede49c93eda62ccf12a9a9d6b91d15
2333174 <1741603127@qq.com>
2018-09-10 21:56:09 +0800

    first

---
+++ HomeWork1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace HomeWork1
+{
+    class P4
+    {
+        static void Main(string[] args)
+        {
+            string s = "";
+            double m = 0;
+            double n = 0.0;
+            s = Console.ReadLine();
+            m = Double.Parse(s);
+            s = Console.ReadLine();
+            n = Double.Parse(s);
+            Console.WriteLine($"Result:{m*n}");
+        }
+    }
+}
---
+++ P5/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace P5
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string s1 = this.textBox1.Text;
+            string s2 = this.textBox2.Text;
+            double m = Double.Parse(s1);
+            double n = Double.Parse(s2);
+            string result = (m * n).ToString();
+            this.textBox3.Text = result;
+        }
+    }
+}
